0123c70a0d37eb545ce41cfaa86144080d8d10f6
Ben Knowles <ben@ionophore.com>
2025-02-16 17:11:22 -0800

    Walk the uia tree of an exe input by the user

---
+++ UiaSpy/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WinRT.UiaSpyVtableClasses;
+
+namespace UiaSpy
+{
+	// Main data structure for app-lifetime data.  Owned by UiaSpy.App
+	public class MainApp
+	{
+		public FlaUI.UIA3.UIA3Automation Automation = new UIA3Automation();
+		public FlaUI.Core.Application AttachedApp;
+		public MainApp()
+		{
+			//...
+		}
+	}
+}
---
+++ UiaSpy/Models/ExePathViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiaSpy.Models
+{
+	public class ExePathViewModel : INotifyPropertyChanged
+	{
+		private string _value = "";
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		public ExePathViewModel()
+		{
+			Value = "D:\\bin\\BabelPad.exe";
+		}
+
+		public string Value
+		{
+			get { return _value; }
+			set {
+				_value = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
