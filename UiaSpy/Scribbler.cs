c7aa47878a31cfe96b998e860f4dd6e1219fe568
Ben Knowles <ben@ionophore.com>
2025-03-01 05:50:55 -0800

    Rectangle disappears after the given duration; positioning and size still not implemented

--- UiaSpy/Scribbler.cs
+++ UiaSpy/Scribbler.cs
@@ -10,14 +10,26 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Windows.Networking.Connectivity;
 
 namespace UiaSpy
 {
+	// TODO:  It's awkward that the timer lives on TransparentWindow; it would be better if i could move it to
+	// Scribbler.
+	// TODO:  Need to .Dispose() the _window (and the _pen ?)
 	public class TransparentWindow : System.Windows.Forms.Form
 	{
+		public System.Windows.Forms.Timer closeTimer;
 		public TransparentWindow()
 		{
 			FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			BackColor = Color.Lime; // Set a unique color for transparency
+			TransparencyKey = Color.Lime; // Make that color transparent
+
+			closeTimer = new System.Windows.Forms.Timer();
+			closeTimer.Tick += CloseTimer_Tick;
+
+			TopMost = true; // Keep it on top
 			ShowInTaskbar = false;
 			Visible = false;
 		}
@@ -31,21 +43,31 @@ namespace UiaSpy
 				createParams.ExStyle |= (int)WindowStyles.WS_EX_TRANSPARENT;
 				createParams.ExStyle |= (int)WindowStyles.WS_EX_TOOLWINDOW;
 				createParams.ExStyle |= (int)WindowStyles.WS_EX_TOPMOST;
-				//createParams.ExStyle |= (int)WindowStyles.WS_EX_LAYERED;
 				return createParams;
 			}
+		}
+
+		public void CloseTimer_Tick(object sender, EventArgs e)
+		{
+			closeTimer.Stop();
+			this.Hide();
+			this.Close();
 		}
 	}
 
 	public class Scribbler
 	{
-		public int Thickness = 5;
 		private TransparentWindow _window = new TransparentWindow();
+
+		private int Thickness = 5;
+		private System.Drawing.Rectangle _rect = new System.Drawing.Rectangle();
+		private System.Drawing.Pen _pen = new System.Drawing.Pen(Color.Red, 3);
+		
+
 		public Scribbler()
 		{
-			//SetLayeredWindowAttributes(_window.Handle, 0, )
+			_window.Paint += new PaintEventHandler(Paint);
 		}
-
 
 		//
 		// P/Invoke decls
@@ -53,52 +75,33 @@ namespace UiaSpy
 		[DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, SetLastError = true)]
 		internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 		[DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, SetLastError = true)]
-
 		[return: MarshalAs(UnmanagedType.Bool)]
 		internal static extern bool SetWindowPos(IntPtr hWnd, IntPtr hwndAfter, int x, int y, int width, int height, int flags);
-
-		/*[DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
-		private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
-
-		[DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
-		private static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, IntPtr dwNewLong);*/
-
-		[DllImport("user32.dll")]
-		public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
 
 		//
 		// Drawing
 		//
 		public void DrawRect(System.Drawing.Rectangle rectangle, System.Drawing.Color color, TimeSpan duration)
 		{
-			var leftBorder = new System.Drawing.Rectangle(rectangle.X, rectangle.Y, Thickness, rectangle.Height);
-			var topBorder = new System.Drawing.Rectangle(rectangle.X, rectangle.Y, rectangle.Width, Thickness);
-			var rightBorder = new System.Drawing.Rectangle(rectangle.X + rectangle.Width - Thickness, rectangle.Y, Thickness, rectangle.Height);
-			var bottomBorder = new System.Drawing.Rectangle(rectangle.X, rectangle.Y + rectangle.Height - Thickness, rectangle.Width, Thickness);
-			var allBorders = new[] { leftBorder, topBorder, rightBorder, bottomBorder };
-
-			var gdiColor = System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
-			_window.BackColor = gdiColor;
-
-			_window.Paint += (s, e) => {
-				using (Pen pen = new Pen(Color.Red, 15))
-				{
-					e.Graphics.DrawRectangle(pen, rectangle);
-				}
-			};
+			_rect = rectangle;
+			//_rect = new Rectangle(50, 50, 200, 100);
+			_window.closeTimer.Interval = (int)duration.TotalMilliseconds;
+			_window.closeTimer.Start();
 
 			SetWindowPos(_window.Handle, new IntPtr(-1), rectangle.X, rectangle.Y,
 				rectangle.Width+5, rectangle.Height+5, SetWindowPosFlags.SWP_NOACTIVATE);
 			ShowWindow(_window.Handle, ShowWindowTypes.SW_SHOWNA);
 
-			/*System.Drawing.Graphics g = _window.CreateGraphics();
-			Pen redPen = new Pen(Color.Red, 15);
-			g.DrawRectangle(redPen, rectangle);*/
-
-			Thread.Sleep(duration);
+			/*Thread.Sleep(duration);
 			_window.Hide();
 			_window.Close();
-			_window.Dispose();
+			_window.Dispose();*/
+		}
+
+		private void Paint(object sender, PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+			g.DrawRectangle(_pen, _rect);
 		}
 	}
 }
